3c31d8d9bebcf4110c4b8c0bcb042c3525e1f5b5
LucasE2996 <moeda.60@gmail.com>
2020-07-18 21:24:58 -0300

    first commit

---
+++ src/test/java/com/restapi/expensetracker/ExpenseTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
