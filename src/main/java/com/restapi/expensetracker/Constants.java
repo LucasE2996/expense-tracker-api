b43084dfcf29479dc1be94489ffda38195879657
lrosa <lrosa@squaretrade.com>
2020-07-21 12:07:25 -0300

    Return JWT token

---
+++ src/main/java/com/restapi/expensetracker/Constants.java
@@ -0,0 +1,12 @@
+package com.restapi.expensetracker;
+
+public class Constants {
+
+    public static final String API_SECRET_KEY = "expensetrackerapikey";
+    public static final long TOKEN_VALIDITY = 2 * 60 * 60 * 1000; // 2hrs
+
+    public static final String EMAIL = "email";
+    public static final String PASSWORD = "password";
+    public static final String FIRST_NAME = "firstName";
+    public static final String LAST_NAME = "lastName";
+}
--- src/main/java/com/restapi/expensetracker/api/UserController.java
+++ src/main/java/com/restapi/expensetracker/api/UserController.java
@@ -1,7 +1,10 @@
 package com.restapi.expensetracker.api;
 
+import com.restapi.expensetracker.Constants;
 import com.restapi.expensetracker.domain.User;
 import com.restapi.expensetracker.service.UserService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,8 +13,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
 
 @RestController
 @RequestMapping("/api/users")
@@ -22,29 +28,37 @@ public class UserController {
 
     @PostMapping("/login")
     public ResponseEntity<Map<String, String>> loginUser(@RequestBody Map<String, Object> userMap) {
-        String email = (String) userMap.get("email");
+        String email = (String) userMap.get(Constants.EMAIL);
         String password = (String) userMap.get("password");
 
         User user = userService.validateUser(email, password);
 
-        Map<String, String> map = new HashMap<>();
-        map.put("message", "loggedIn successfully");
-
-        return new ResponseEntity<>(map, HttpStatus.OK);
+        return new ResponseEntity<>(generateJWTToken(user), HttpStatus.OK);
     }
 
     @PostMapping("/register")
     public ResponseEntity<Map<String, String>> registerUser(@RequestBody Map<String, Object> userMap) {
-        String firstName = (String) userMap.get("firstName");
-        String lastName = (String) userMap.get("lastName");
-        String email = (String) userMap.get("email");
-        String password = (String) userMap.get("password");
+        String firstName = (String) userMap.get(Constants.FIRST_NAME);
+        String lastName = (String) userMap.get(Constants.LAST_NAME);
+        String email = (String) userMap.get(Constants.EMAIL);
+        String password = (String) userMap.get(Constants.PASSWORD);
 
         User user = userService.registerUser(firstName, lastName, email, password);
 
-        Map<String, String> map = new HashMap<>();
-        map.put("message", "registered successfully");
+        return new ResponseEntity<>(generateJWTToken(user), HttpStatus.CREATED);
+    }
 
-        return new ResponseEntity<>(map, HttpStatus.CREATED);
+    private Map<String, String> generateJWTToken(User user) {
+        long timestamp = System.currentTimeMillis();
+        String token = Jwts.builder().signWith(SignatureAlgorithm.HS256, Constants.API_SECRET_KEY)
+                .setIssuedAt(new Date(timestamp))
+                .setExpiration(new Date(timestamp + Constants.TOKEN_VALIDITY))
+                .claim("userId", user.getUserId())
+                .claim(Constants.EMAIL, user.getEmail())
+                .claim(Constants.FIRST_NAME, user.getFirstName())
+                .claim(Constants.LAST_NAME, user.getLastName())
+                .compact();
+
+        return ImmutableMap.of("token", token);
     }
 }
